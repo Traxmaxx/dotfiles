5e9636702288745e6e54e3da29f03631b9225a09
Traxmaxx <320272+Traxmaxx@users.noreply.github.com>
2025-06-11 09:14:55 +0200

    check if files exist before importing and clean up unneeded parts

--- .config.d/fish/conf.d/prompt.fish
+++ .config.d/fish/conf.d/prompt.fish
@@ -14,12 +14,11 @@ set fish_greeting
 set -e aws_profile
 
 if not set -q aws_completer_path
-    #(type -P aws_completer 2> /dev/null)
   set -g aws_completer_path "/opt/homebrew/bin/aws_completer"
     or echo "aws: unable to find aws_completer, completions unavaliable"
 end
 
-# I override the path tide displays in the prompt
+# Override the path tide displays in the prompt
 # The default contains all directories and it gets too long
 # Since tide has no configuration for this, I need to override the function
 set_color -o $tide_pwd_color_anchors | read -l color_anchors
@@ -40,54 +39,3 @@ eval "function _tide_pwd
     end
     string join -- / \"$reset_to_color_dirs\$split_output[1]\$(basename (prompt_pwd))\"
 end"
-
-
-# I converted the old version infos prompt but tide already supports these
-# So they stay disable as long as I will continute to use tide
-# set -U tide_ruby_prompt_color $fish_color_param
-# set -U tide_ruby_prompt_bg_color normal
-# function _tide_item_ruby_prompt
-#   asdf current ruby | string sub -s 17 -e 32 | string trim
-# end
-
-# set -U tide_rust_prompt_color $fish_color_param
-# set -U tide_rust_prompt_bg_color normal
-# function _tide_item_rust_prompt
-#   asdf current rust | string sub -s 17 -e 32 | string trim
-# end
-
-# set -U tide_nodejs_prompt_color $fish_color_param
-# set -U tide_nodejs_prompt_bg_color normal
-# function _tide_item_nodejs_prompt
-#   asdf current nodejs | string sub -s 17 -e 32 | string trim
-# end
-
-
-# Just calculate these once, to save a few cycles when displaying the prompt
-# if not set -q __fish_prompt_hostname
-#   set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-# end
-
-
-# replaced with hydro and it seems to not support manual right prompt things yet
-# at least not without doing everything manually
-# https://github.com/jorgebucaran/hydro/issues/34
-# function fish_prompt
-#   set last_status $status
-#   set_color $fish_color_cwd
-#   printf '%s' (prompt_pwd)
-#   set_color normal
-#   printf '%s > '
-# end
-
-# function fish_right_prompt
-#   set_color $fish_color_param
-#   printf '%s' (__rust_prompt)
-#   set_color $fish_color_normal
-#   printf '|'
-#   set_color $fish_color_param
-#   printf '%s' (__nodejs_prompt)
-#   set_color $fish_color_normal
-#   # see above comment on function fish_prompt
-#   # printf '%s' (__fish_git_prompt)
-# end
