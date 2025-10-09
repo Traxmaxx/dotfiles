cc4a0e579d6da9898e7a3ceeaa373e7dd3e18ee0
Traxmaxx <320272+Traxmaxx@users.noreply.github.com>
2025-10-09 16:27:25 +0200

    setup vim-kitty-navigator

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,36 +3,41 @@
 -- Add any additional keymaps here
 
 local function has_words_before()
-	local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 keymap = {
-	preset = "enter",
-	["<C-y>"] = { "select_and_accept" },
-	["<Tab>"] = {
-		"select_next",
-		"snippet_forward",
-		function(cmp)
-			if has_words_before() or vim.api.nvim_get_mode().mode == "c" then
-				return cmp.show()
-			end
-		end,
-		"fallback",
-	},
-	["<S-Tab>"] = {
-		"select_prev",
-		"snippet_backward",
-		function(cmp)
-			if vim.api.nvim_get_mode().mode == "c" then
-				return cmp.show()
-			end
-		end,
-		"fallback",
-	},
-	completion = {
-		list = {
-			selection = { preselect = false, auto_insert = true },
-		},
-	},
+  preset = "enter",
+  ["<C-y>"] = { "select_and_accept" },
+  ["<Tab>"] = {
+    "select_next",
+    "snippet_forward",
+    function(cmp)
+      if has_words_before() or vim.api.nvim_get_mode().mode == "c" then
+        return cmp.show()
+      end
+    end,
+    "fallback",
+  },
+  ["<S-Tab>"] = {
+    "select_prev",
+    "snippet_backward",
+    function(cmp)
+      if vim.api.nvim_get_mode().mode == "c" then
+        return cmp.show()
+      end
+    end,
+    "fallback",
+  },
+  completion = {
+    list = {
+      selection = { preselect = false, auto_insert = true },
+    },
+  },
+
+  ["<C-h>"] = { ":KittyNavigateLeft<CR>", noremap = true, silent = true, desc = "Kitty Navigate Left" },
+  ["<C-j>"] = { ":KittyNavigateDown<CR>", noremap = true, silent = true, desc = "Kitty Navigate Down" },
+  ["<C-k>"] = { ":KittyNavigateUp<CR>", noremap = true, silent = true, desc = "Kitty Navigate Up" },
+  ["<C-l>"] = { ":KittyNavigateRight<CR>", noremap = true, silent = true, desc = "Kitty Navigate Right" },
 }
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+vim.g.kitty_navigator_password = "slkIekh!dhf@sup#"
+vim.g.kitty_navigator_no_mappings = 1
