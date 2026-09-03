060a1eaa471f7557766a948da102cbd5b0521870
Traxmaxx <320272+Traxmaxx@users.noreply.github.com>
2026-09-03 11:21:46 +0200

    change ~ to $HOME

--- .config/fish/completions/aws.fish
+++ .config/fish/completions/aws.fish
@@ -1,9 +1,16 @@
-if type -q aws_completer
-    function __aws_completer
-        set -lx COMP_LINE (commandline -pc)
-        set -lx COMP_POINT (string length $COMP_LINE)
-        aws_completer | tr -d ' '
+function __aws_complete
+  if set -q aws_completer_path
+    set -lx COMP_SHELL fish
+    set -lx COMP_LINE (commandline -opc)
+
+    if string match -q -- "-*" (commandline -opt)
+      set COMP_LINE $COMP_LINE -
     end
 
-    complete -c aws -f -a "(__aws_completer)"
+    eval $aws_completer_path | command sed 's/ $//'
+  end
 end
+
+complete --command aws --no-files --arguments '(__aws_complete)'
+complete --command aws --no-files --condition 'test (count (commandline -opc)) -lt 2' --arguments profile -d 'Get or set current profile'
+complete --command aws --no-files --condition '__fish_seen_subcommand_from profile' --arguments '(aws profiles)'
